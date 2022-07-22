e15d0d192527b9a628d97a4d1cb819b39ffaf4e2
GGshor <73340248+GGshor@users.noreply.github.com>
2022-07-22 14:07:57 +0200

    Add promise

--- Tests/GetFirebase.server.lua
+++ Tests/GetFirebase.server.lua
@@ -1,5 +1,4 @@
 local ServerStorage = game:GetService("ServerStorage")
 
+local Promise = require(ServerStorage.Promise)
 local FirebaseService = require(ServerStorage.FirebaseService)
-
-local Database = FirebaseService
